52b805051c1b3878d1d035f25fb5488653166107
DevSimran <devsimranjeet@hotmail.com>
2024-02-04 23:51:41 +0530

    all setup configured

--- src/utils/asynchandller.js
+++ src/utils/asynchandller.js
@@ -1,5 +1,5 @@
 const asyncHandller = (requestHandler)=> {
-    (req, res, next)=>{
+    return (req, res, next)=>{
         Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
